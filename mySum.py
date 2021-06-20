adb220ac16ba1a8ec455bd89713c2ea5afd6738c
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-06-20 12:56:23 +0100

    adds a second argument which defines the starting point of the suming

--- mySum.py
+++ mySum.py
@@ -1,12 +1,12 @@
 # A function to simulate the in_built sum function
 
-def my_sum(*numbers):
+def my_sum(numbers, begin=0):
     """returns the sum of the positional arguments given"""
-    result = 0
+    result = begin
     for number in numbers:
         result += number
     print(result)
     return result
 
 
-my_sum(100, 200, 300, 400)
+my_sum([100, 200, 300, 600])
