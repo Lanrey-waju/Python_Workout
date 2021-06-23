8c4a2aec6b8d3c2cc9b2b4b5566550789896808a
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-06-23 18:46:12 +0100

    use .join() operator instead of += on to create a string

--- pl_sentence.py
+++ pl_sentence.py
@@ -2,15 +2,15 @@ def pl_sentence():
     """
     A pig latin translator 2.0. Takes a simple sentence and then outputs the pig latin translator of it.
     """
+    output = []
     raw_input = input("Enter the text to be translated: ").lower().strip()
     split_word = raw_input.split()
     for word in split_word:
         if word[0] in 'aeiou':
-            print(f'{word}way'.title(), end=' ')
-            # return f'{word}way'.title()
+            output.append(f'{word}way'.title())
         else:
-            print(f'{word[1:]}{word[0]}ay'.title(), end=" ")
-            # return f'{word[1:]}{character}ay'.title()
+            output.append(f'{word[1:]}{word[0]}ay'.title())
+    return ' '.join(output)
 
 
-pl_sentence()
+print(pl_sentence())
