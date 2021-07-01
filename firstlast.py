0f91b09f7d8cc116debf8057d0ffff216ec5a1e8
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-07-01 22:59:43 +0100

    initial commit

---
+++ firstlast.py
@@ -0,0 +1,14 @@
+import operator
+
+
+def firstLast(iterable):
+    """
+    A program to return the first and the last items in an iterable
+    :param iterable: string, list, tuple
+    :return: first and last occurrence in the iterable
+    """
+    outcome = operator.itemgetter(0, -1)
+    return outcome(iterable)
+
+
+print(firstLast([0, 2, 3, 5]))
