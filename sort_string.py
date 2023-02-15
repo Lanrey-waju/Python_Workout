a020a804bfc888332c7e15651d9dc42d8d1f0f79
Abdulmumin <lanreywaju97@gmail.com>
2023-02-16 00:26:37 +0100

    String sorting

--- sort_string.py
+++ sort_string.py
@@ -4,3 +4,17 @@ def strsort(seq):
 
 
 print(strsort("wyfgilkdpsjw"))
+
+
+def sort_and_comma(seq):
+    """
+    Break a given sentence into individual words, and then sort
+    those words alphabetically before printing the words separated by comma
+    seq: str
+    Returns: str
+    """
+    sorted_words = sorted(seq.split())
+    return ", ".join(sorted_words)
+
+
+print(sort_and_comma("World Hello Dick Harry"))
